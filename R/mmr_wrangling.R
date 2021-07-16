5ecab500a7f825b964bcb38f3bf95b94a23f20d5
Jesse Mostipak <jesse.maegan@gmail.com>
2021-07-15 20:42:28 -0500

    created package and wrote data to file

--- R/mmr_wrangling.R
+++ R/mmr_wrangling.R
@@ -7,19 +7,21 @@ library(here)
 raw_data <- read_json(here("data/sitedata.json"))
 
 # unnest our lists and create a tibble - THANK YOU TAN <3
-data <- raw_data %>% 
-  purrr::pluck("patients") %>% 
-  tibble::tibble() %>% 
+data <- raw_data %>%
+  purrr::pluck("patients") %>%
+  tibble::tibble() %>%
   tidyr::unnest_wider(1)
 
 # a little bit of data wrangling
-marine_mammals <- data %>% 
+marine_mammals <- data %>%
   mutate(sex = as.factor(sex),
          collection_site = as.factor(collection_site),
          reason_for_admit = str_trim(reason_for_admit),
          admit_date = date(admit_date),
          admit_weight = as.numeric(admit_weight),
-         release_date = ymd(release_date), 
+         release_date = ymd(release_date),
          release_site = as.factor(release_site),
          release_weight = as.numeric(release_weight),
-         transfer_date = ymd(transfer_date)) 
+         transfer_date = ymd(transfer_date))
+
+usethis::use_data(marine_mammals, overwrite = TRUE)
